392cdb5e36b7f958771da2a71843f7bcda215fb8
Mohamed Rilwan <mohamed.rilwan@werkspot.nl>
2023-01-22 20:10:45 +0100

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,19 +4,25 @@ import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
+import { GetStaticProps } from 'next'
 
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>knowledge is that which benefits not that which is memorized.</p>
+        
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
@@ -36,7 +42,7 @@ export default function Home({ allPostsData }) {
   )
 }
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData()
   return {
     props: {
---
+++ posts/hello-world.md
@@ -0,0 +1,6 @@
+---
+title: 'Hello World'
+date: '2023-01-01'
+---
+
+Welcome to my personal blog.
